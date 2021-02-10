0a7c011a1c925a7b696b1640ca4df2933dd4e1be
Tanmay Verma <tanmay2592@gmail.com>
2021-02-09 20:35:07 -0800

    Fix the build to include necessary includes and library files

--- tools/gen_openvino_dockerfile.py
+++ tools/gen_openvino_dockerfile.py
@@ -88,10 +88,19 @@ RUN wget ${INTEL_COMPUTE_RUNTIME_URL}/intel-gmmlib_19.3.2_amd64.deb && \
 #
 WORKDIR /opt/openvino
 
+RUN mkdir -p /opt/openvino/include && \
+   (cd /opt/openvino/include && \
+     cp -r /opt/intel/openvino_${OPENVINO_VERSION}/deployment_tools/inference_engine/include/* \
+          .)
+
 RUN mkdir -p /opt/openvino/lib && \
     cp -r /opt/intel/openvino_${OPENVINO_VERSION}/licensing \
           /opt/openvino/LICENSE.openvino && \
+    cp /opt/intel/openvino_${OPENVINO_VERSION}/deployment_tools/inference_engine/lib/intel64/plugins.xml \
+       /opt/openvino/lib && \
     cp /opt/intel/openvino_${OPENVINO_VERSION}/deployment_tools/inference_engine/lib/intel64/libinference_engine.so \
+       /opt/openvino/lib && \
+    cp /opt/intel/openvino_${OPENVINO_VERSION}/deployment_tools/inference_engine/lib/intel64/libinference_engine_legacy.so \
        /opt/openvino/lib && \
     cp /opt/intel/openvino_${OPENVINO_VERSION}/deployment_tools/inference_engine/lib/intel64/libinference_engine_transformations.so \
        /opt/openvino/lib && \
