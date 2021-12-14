6a95f5e4dfbb8b4a7a935937f9698ab8b1d251c2
David Goodwin <davidg@nvidia.com>
2021-12-14 14:57:04 -0800

    Responder should explicitly use 'first dimension batching'

--- src/openvino.cc
+++ src/openvino.cc
@@ -1127,8 +1127,8 @@ ModelInstanceState::ReadOutputTensors(
     std::vector<TRITONBACKEND_Response*>* responses)
 {
   BackendOutputResponder responder(
-      requests, request_count, responses, model_state_->MaxBatchSize(),
-      model_state_->TritonMemoryManager(), model_state_->EnablePinnedInput(),
+      requests, request_count, responses, model_state_->TritonMemoryManager(),
+      model_state_->MaxBatchSize() > 0, model_state_->EnablePinnedInput(),
       CudaStream());
 
   bool cuda_copy = false;
